28c36b4348789da3989ddbe481c6f12a4c23508d
Miguel Angel Berenguer Alcaine <mberenguer@addingtechnology.es>
2023-11-19 16:33:02 +0100

    Añadido JavaScript para la galería de imagenes

--- src/app.js
+++ src/app.js
@@ -1,2 +1,27 @@
-const name = 'world';
-console.log(`Hello ${name}`);
+// Obtén la lista de imágenes y los puntos indicadores de página
+const images = document.querySelectorAll('.gallery > div > picture');
+const points = document.querySelectorAll('.gallery > span > a');
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      const imageId = entry.target.id;
+      highlightLink(imageId);
+    }
+  });
+}, { threshold: 0.5 });
+
+images.forEach(image => {
+  observer.observe(image);
+});
+
+function highlightLink(imageId) {
+  points.forEach(link => {
+    link.style.backgroundColor = '#aaa';
+  });
+
+  const correspondingLink = document.querySelector(`.gallery > span > a[href="#${imageId}"]`);
+  if (correspondingLink) {
+    correspondingLink.style.backgroundColor = '#888';
+  }
+}
